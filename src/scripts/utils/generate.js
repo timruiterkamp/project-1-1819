4369b6c8d326c51cf92ff6eb4fa67a63734d0621
Tim <timruiterkamp@gmail.com>
2019-03-08 12:28:11 +0100

    error handling wrapper errors

--- src/scripts/utils/generate.js
+++ src/scripts/utils/generate.js
@@ -77,50 +77,62 @@ export default class Generate extends Component {
     appendParent.innerHTML = "";
     const w = this.w;
 
-    data.map(res => {
-      if (res.length < 1) {
-        appendParent.appendChild(
-          this.dom.create(w("h2", { class: "no-results" }, "geen resultaten"))
-        );
-      } else {
-        const content = w(
-          "div",
-          { class: "search-items" },
-          ...res.map(result => {
-            return result && result !== undefined
-              ? w(
-                  "div",
-                  {
-                    class: "search-item"
-                  },
+    try {
+      data.map(res => {
+        if (res.length < 1) {
+          appendParent.appendChild(
+            this.dom.create(w("h2", { class: "no-results" }, "geen resultaten"))
+          );
+        } else {
+          const content = w(
+            "div",
+            { class: "search-items" },
+            ...res.map(result => {
+              return result && result !== undefined
+                ? w(
+                    "div",
+                    {
+                      class: "search-item"
+                    },
 
-                  result.images && result.images !== undefined
-                    ? result.images[1]
-                      ? w("img", {
-                          src: result.images[1]
-                        })
-                      : w("img", {
-                          src: result.images[0]
-                        })
-                    : w("div", { class: "search-image-placeholder" }),
-                  result.title && result.title.full !== undefined
-                    ? w("h4", {}, result.title.full)
-                    : w("h4", {}, "Titel: niet bekend"),
-                  result.author && result.author.fullname !== undefined
-                    ? w("p", {}, "Auteur: " + result.author.fullname)
-                    : w("p", {}, "Auteur: niet bekend"),
-                  result.summary && result.summary !== undefined
-                    ? w("p", {}, "Samenvatting: " + result.summary)
-                    : w("p", {}, "Samenvatting: niet bekend"),
-                  result.format && result.format !== undefined
-                    ? w("p", {}, "format: " + result.format)
-                    : w("p", {}, "Format: niet bekend")
-                )
-              : "";
-          })
-        );
-        appendParent.appendChild(this.dom.create(content));
-      }
-    });
+                    result.images && result.images !== undefined
+                      ? result.images[1]
+                        ? w("img", {
+                            src: result.images[1]
+                          })
+                        : w("img", {
+                            src: result.images[0]
+                          })
+                      : w("div", { class: "search-image-placeholder" }),
+                    result.title && result.title.full !== undefined
+                      ? w("h4", {}, result.title.full)
+                      : w("h4", {}, "Titel: niet bekend"),
+                    result.author && result.author.fullname !== undefined
+                      ? w("p", {}, "Auteur: " + result.author.fullname)
+                      : w("p", {}, "Auteur: niet bekend"),
+                    result.summary && result.summary !== undefined
+                      ? w("p", {}, "Samenvatting: " + result.summary)
+                      : w("p", {}, "Samenvatting: niet bekend"),
+                    result.format && result.format !== undefined
+                      ? w("p", {}, "format: " + result.format)
+                      : w("p", {}, "Format: niet bekend")
+                  )
+                : "";
+            })
+          );
+          appendParent.appendChild(this.dom.create(content));
+        }
+      });
+    } catch {
+      appendParent.appendChild(
+        this.dom.create(
+          w(
+            "h2",
+            { class: "no-results" },
+            "Er is iets fout gegaan, probeer een ander zoekwoord"
+          )
+        )
+      );
+    }
   }
 }
